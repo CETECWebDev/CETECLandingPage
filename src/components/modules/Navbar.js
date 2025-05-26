64caf05621a71e186581c0bca386ae4de86565c0
Ali-Asadian-456 <142652407+Ali-Asadian-456@users.noreply.github.com>
2025-05-27 01:16:29 +0330

    style fixed

--- src/components/modules/Navbar.js
+++ src/components/modules/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         
         <div className="flex items-center space-x-2 rtl:space-x-reverse">
-          <img src="/logodark.svg" alt="logo" className="h-10 w-auto" />
+          <img src="/logodark.svg" alt="logo" className="h-[50px] w-[150px]" />
         
         </div>
 
@@ -26,20 +26,22 @@ export default function Navbar() {
           </li>
 
         
-          <div className="w-6" />
+          
 
         
-          <li className="flex items-center cursor-pointer hover:text-orange-400 space-x-1 rtl:space-x-reverse">
+          <li className=" text-[20px] font-medium flex items-center cursor-pointer hover:text-orange-400 space-x-1 rtl:space-x-reverse">
             <span>قطب دانش</span>
             <FaChevronDown className="text-white" />
           </li>
         </ul>
 
       
-        <button className="text-[24px] font-medium border border-orange-500 text-white px-4 py-2 rounded-full hover:border-white transition text-sm">
+        <button className="text-[24px] font-medium border border-[var(--primary2)] text-white px-[24px] py-[11px] rounded-[25px] hover:border-white transition">
           ثبت‌نام / ورود
         </button>
-      </div>
+
+        </div>
+      
     </nav>
   );
 }
