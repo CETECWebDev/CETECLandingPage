0df7fef64183077a8e03bc27e63f60ea22e7cffe
Ali-Asadian-456 <142652407+Ali-Asadian-456@users.noreply.github.com>
2025-05-28 05:56:27 +0330

    fixed

--- src/components/modules/Wave.js
+++ src/components/modules/Wave.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function Wave() {
   return (
-    <div className="w-full overflow-hidden h-[200px] sm:h-[300px]">
+    <div className="w-full">
       <img
         src="wave.svg"
         alt=""
-        className="w-[150%] sm:w-full h-full object-cover object-center"
+        className="w-[150%]  object-cover object-center"
       />
     </div>
   );
