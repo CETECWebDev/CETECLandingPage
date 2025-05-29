3c58f63c001a506455da9fe9a28a4e1dac859455
Ali-Asadian-456 <142652407+Ali-Asadian-456@users.noreply.github.com>
2025-05-29 12:48:37 +0330

    wave added

--- src/components/modules/Wave.js
+++ src/components/modules/Wave.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Wave() {
   return (
-    <div className="custom-banner-wave w-full ">
+    <div className="custom-banner-wave h-[380px] w-full ">
 
     </div>
   );
