8ea0c7eb963078f48469062df1e361e2d94f6ab9
Ali-Asadian-456 <142652407+Ali-Asadian-456@users.noreply.github.com>
2025-05-31 10:21:46 +0330

    .

--- src/components/modules/AdvicingDotLayout.js
+++ src/components/modules/AdvicingDotLayout.js
@@ -5,7 +5,7 @@ export default function AdvicingDotLayout({right , top}) {
     const dots = new Array(25).fill("");
 
   return (
-    <div className={`w-[97px] h-[100px] grid grid-cols-5 place-items-center absolute right-[50px] top-[-50px]`}>
+    <div className={`w-[97px] h-[120px] grid grid-cols-5 place-items-center absolute right-[50px] top-[-50px]`}>
         {dots.map( (dot,index) => {
             return <div key={index} className='bg-white w-[5px] h-[5px] rounded-full'></div>
         })}
