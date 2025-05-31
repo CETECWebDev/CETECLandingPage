d1595a3c79ea6ea3152006494194335a32e663d1
Ali-Asadian-456 <142652407+Ali-Asadian-456@users.noreply.github.com>
2025-05-31 22:01:01 +0330

    style products changed

--- src/components/modules/Products.js
+++ src/components/modules/Products.js
@@ -13,53 +13,25 @@ function Products() {
           </p>
         </div>
 
-        <div className="grid md:grid-cols-2 items-center gap-8 mb-20 mt-10">
-          <div className="flex flex-row items-center justify-start gap-4">
-            <div className="w-[25px] h-[350px] bg-orange-500 rounded" />
-            <div>
+        <div className="grid  md:grid-cols-2 items-center gap-8   mx-auto mb-20">
+
+          <div className="  flex  items-center justify-start h-[350px]">
+             <div className="w-[25px] h-full  bg-orange-500 " />
+            <div className="h-full flex justify-center items-center bg-[#030F3F]">
               <p
-                className="text-[22px] font-medium text-orange-500 tracking-wider"
+                className=" text-center text-[26px] px-2 font-medium text-orange-400 tracking-wider"
                 style={{ writingMode: "vertical-rl", textOrientation: "mixed" }}
               >
-                smart vertical take_off drone
+                smart vertical <br/> take_off drone
               </p>
             </div>
-            <div className="bg-[#00143B] rounded-xl p-6 w-full max-w-xs text-center">
-              <img src="/img1.jpg" alt="drone" className="mx-auto h-24 mb-4" />
-              <p className="text-[10px] font-normal text-green-600">
-                پهپاد عمود پرواز هوشمند
-              </p>
-              <p className="text-[24px] font-semibold text-green-400 mt-1">
-                ورژن ۱.۲
-              </p>
+            <div className="bg-[#020A2A] px-6 h-full w-full grid place-content-center gap-6  max-w-[434px] text-center">
+              <img src="/img1.jpg" alt="drone" className="mx-auto h-24 " />
+              <p className="text-[20px] font-normal">پهپاد عمود پرواز هوشمند</p>
+              <p className="text-[24px] font-medium text-green-600">ورژن ۱.۲</p>
             </div>
           </div>
-          <div>
-            <h2 className="text-[32px] font-semibold mb-2">
-              پهپاد عمود پرواز هوشمند{" "}
-              <span className="text-green-500">ورژن ۱.۲</span>
-            </h2>
-            <p className="text-[24px] font-medium text-gray-300 leading-relaxed mb- mt-3 pl=12">
-              این محصول اولین نسل پهپادهای هوشمند عمودپرواز تولید شده در ایران
-              است. دارای بخش کنترل پرواز پیشرفته‌ای است که آن را برای کاربردهای
-              بسیاری از جمله امداد و نجات، مدیریت بحران، بازرسی و امنیت و
-              سامانه‌های مدار بسته و... مناسب می‌سازد. به راحتی تنظیم و
-              راه‌اندازی می‌شود.
-            </p>
-            <button
-              className="text-[18px] font-medium mt-8
-        inner-orange-shadow    px-6 py-2 rounded-lg hover:bg-[var(--dark-light-primary)] hover:text-white transition"
-            >
-              اطلاعات بیشتر
-            </button>
-          </div>
-        </div>
 
-
-
-
-       
-        <div className="grid md:grid-cols-2 items-center gap-8  mx-auto mb-20">
           <div className="w-[652px] h-[303px] px-12 py-6">
             <h2 className="text-[32px] font-semibold mb-4">
               پهپاد عمود پرواز هوشمند{" "}
@@ -82,52 +54,22 @@ function Products() {
             </button>
           </div>
 
-          <div className="flex flex-row-reverse items-center justify-start gap-4">
-             <div className="w-[25px] h-[350px] bg-orange-500 rounded" />
-            <div>
-              <p
-                className="text-[22px] font-medium text-orange-400 tracking-wider"
-                style={{ writingMode: "vertical-rl", textOrientation: "mixed" }}
-              >
-                smart vertical take_off drone
-              </p>
-            </div>
-            <div className="bg-[#00143B] rounded-xl p-6 w-full max-w-xs text-center">
-              <img src="/img1.jpg" alt="drone" className="mx-auto h-24 mb-4" />
-              <p className="text-[20px] font-normal">پهپاد عمود پرواز هوشمند</p>
-              <p className="text-[24px] font-medium text-green-600">ورژن ۱.۲</p>
-            </div>
-          </div>
+
         </div>
 
+
+
+
        
-        <div className="grid md:grid-cols-2 items-center gap-8 mx-auto mb-20">
-          <div className="flex flex-row items-center justify-start gap-4">
-            <div className="w-[25px] h-[350px] bg-orange-500 rounded" />
-            <div>
-              <p
-                className="text-[22px] font-medium text-orange-500 tracking-wider"
-                style={{ writingMode: "vertical-rl", textOrientation: "mixed" }}
-              >
-                smart vertical take_off drone
-              </p>
-            </div>
-            <div className="bg-[#00143B] rounded-xl p-6 w-full max-w-xs text-center">
-              <img src="/img1.jpg" alt="drone" className="mx-auto h-24 mb-4" />
-              <p className="text-[10px] font-normal text-green-600">
-                پهپاد عمود پرواز هوشمند
-              </p>
-              <p className="text-[24px] font-semibold text-green-400 mt-1">
-                ورژن ۱.۲
-              </p>
-            </div>
-          </div>
+        <div className="grid md:grid-cols-2 items-center gap-8   mx-auto mb-20">
           <div className="w-[652px] h-[303px] px-12 py-6">
-            <h2 className="text-[32px] font-semibold mb-2">
+            <h2 className="text-[32px] font-semibold mb-4">
               پهپاد عمود پرواز هوشمند{" "}
-              <span className="text-green-500">ورژن ۱.۲</span>
+              <span className="text-[32px] font-semibold text-gray-400">
+                ورژن ۱.۲
+              </span>
             </h2>
-            <p className="text-[24px] font-medium text-gray-300 leading-relaxed mb-6">
+            <p className="text-[24px] font-medium text-gray-300 leading-relaxed mb-6 mt-3 pr-6">
               این محصول اولین نسل پهپادهای هوشمند عمودپرواز تولید شده در ایران
               است. دارای بخش کنترل پرواز پیشرفته‌ای است که آن را برای کاربردهای
               بسیاری از جمله امداد و نجات، مدیریت بحران، بازرسی و امنیت و
@@ -141,77 +83,53 @@ function Products() {
               اطلاعات بیشتر
             </button>
           </div>
-        </div>
 
-        
-        <div className="grid md:grid-cols-2 items-center gap-8 mx-auto mb-20">
-          <div className="w-[652px] h-[303px] px-12 py-6">
-            <h2 className="text-[32px] font-semibold mb-2">
-              پهپاد عمود پرواز هوشمند{" "}
-              <span className="text-[32px] font-semibold text-gray-400">
-                ورژن ۱.۲
-              </span>
-            </h2>
-            <p className="text-[24px] font-medium text-gray-300 leading-relaxed mb-6">
-              این محصول اولین نسل پهپادهای هوشمند عمودپرواز تولید شده در ایران
-              است. دارای بخش کنترل پرواز پیشرفته‌ای است که آن را برای کاربردهای
-              بسیاری از جمله امداد و نجات، مدیریت بحران، بازرسی و امنیت و
-              سامانه‌های مدار بسته و... مناسب می‌سازد. به راحتی تنظیم و
-              راه‌اندازی می‌شود.
-            </p>
-            <button
-              className="text-[18px] font-medium mt-8
-        inner-orange-shadow    px-6 py-2 rounded-lg hover:bg-[var(--dark-light-primary)] hover:text-white transition"
-            >
-              اطلاعات بیشتر{" "}
-            </button>
-          </div>
-
-          <div className="flex flex-row-reverse items-center justify-start gap-4">
-             <div className="w-[25px] h-[350px] bg-orange-500 rounded" />
-            <div classNaame="w-[652px] h-[303px] px-12 py-6">
+          <div className="  flex flex-row-reverse  items-center justify-start h-[350px]">
+             <div className="w-[25px] h-full  bg-orange-500 " />
+            <div className="h-full flex justify-center items-center bg-[#030F3F]">
               <p
-                className="text-[22px] font-medium text-orange-400 tracking-wider"
+                className=" text-center text-[26px] px-2 font-medium text-orange-400 tracking-wider"
                 style={{ writingMode: "vertical-rl", textOrientation: "mixed" }}
               >
-                smart vertical take_off drone
+                smart vertical <br/> take_off drone
               </p>
             </div>
-            <div className="bg-[#00143B] rounded-xl p-6 w-full max-w-xs text-center">
-              <img src="/img1.jpg" alt="drone" className="mx-auto h-24 mb-4" />
+            <div className="bg-[#020A2A] px-6 h-full w-full grid place-content-center gap-6  max-w-[434px] text-center">
+              <img src="/img1.jpg" alt="drone" className="mx-auto h-24 " />
               <p className="text-[20px] font-normal">پهپاد عمود پرواز هوشمند</p>
               <p className="text-[24px] font-medium text-green-600">ورژن ۱.۲</p>
             </div>
           </div>
         </div>
 
-        <div className="grid md:grid-cols-2 items-center gap-8 mx-auto mb-20">
-          <div className="flex flex-row items-center justify-start gap-4">
-            <div className="w-[25px] h-[350px] bg-orange-500 rounded" />
-            <div>
+       
+        <div className="grid  md:grid-cols-2 items-center gap-8   mx-auto mb-20">
+
+          <div className="  flex  items-center justify-start h-[350px]">
+             <div className="w-[25px] h-full  bg-orange-500 " />
+            <div className="h-full flex justify-center items-center bg-[#030F3F]">
               <p
-                className="text-[22px] font-medium text-orange-500 tracking-wider"
+                className=" text-center text-[26px] px-2 font-medium text-orange-400 tracking-wider"
                 style={{ writingMode: "vertical-rl", textOrientation: "mixed" }}
               >
-                smart vertical take_off drone
+                smart vertical <br/> take_off drone
               </p>
             </div>
-            <div className="bg-[#00143B] rounded-xl p-6 w-full max-w-xs text-center">
-              <img src="/img1.jpg" alt="drone" className="mx-auto h-24 mb-4" />
-              <p className="text-[10px] font-normal text-green-600">
-                پهپاد عمود پرواز هوشمند
-              </p>
-              <p className="text-[24px] font-semibold text-green-400 mt-1">
-                ورژن ۱.۲
-              </p>
+            <div className="bg-[#020A2A] px-6 h-full w-full grid place-content-center gap-6  max-w-[434px] text-center">
+              <img src="/img1.jpg" alt="drone" className="mx-auto h-24 " />
+              <p className="text-[20px] font-normal">پهپاد عمود پرواز هوشمند</p>
+              <p className="text-[24px] font-medium text-green-600">ورژن ۱.۲</p>
             </div>
           </div>
-          <div>
-            <h2 className="text-[32px] font-semibold mb-2">
+
+          <div className="w-[652px] h-[303px] px-12 py-6">
+            <h2 className="text-[32px] font-semibold mb-4">
               پهپاد عمود پرواز هوشمند{" "}
-              <span className="text-green-500">ورژن ۱.۲</span>
+              <span className="text-[32px] font-semibold text-gray-400">
+                ورژن ۱.۲
+              </span>
             </h2>
-            <p className="text-[24px] font-medium text-gray-300 leading-relaxed mb-6">
+            <p className="text-[24px] font-medium text-gray-300 leading-relaxed mb-6 mt-3 pr-6">
               این محصول اولین نسل پهپادهای هوشمند عمودپرواز تولید شده در ایران
               است. دارای بخش کنترل پرواز پیشرفته‌ای است که آن را برای کاربردهای
               بسیاری از جمله امداد و نجات، مدیریت بحران، بازرسی و امنیت و
@@ -222,9 +140,97 @@ function Products() {
               className="text-[18px] font-medium mt-8
         inner-orange-shadow    px-6 py-2 rounded-lg hover:bg-[var(--dark-light-primary)] hover:text-white transition"
             >
-              اطلاعات بیشتر{" "}
+              اطلاعات بیشتر
             </button>
           </div>
+
+
+        </div>
+
+        
+        <div className="grid md:grid-cols-2 items-center gap-8   mx-auto mb-20">
+          <div className="w-[652px] h-[303px] px-12 py-6">
+            <h2 className="text-[32px] font-semibold mb-4">
+              پهپاد عمود پرواز هوشمند{" "}
+              <span className="text-[32px] font-semibold text-gray-400">
+                ورژن ۱.۲
+              </span>
+            </h2>
+            <p className="text-[24px] font-medium text-gray-300 leading-relaxed mb-6 mt-3 pr-6">
+              این محصول اولین نسل پهپادهای هوشمند عمودپرواز تولید شده در ایران
+              است. دارای بخش کنترل پرواز پیشرفته‌ای است که آن را برای کاربردهای
+              بسیاری از جمله امداد و نجات، مدیریت بحران، بازرسی و امنیت و
+              سامانه‌های مدار بسته و... مناسب می‌سازد. به راحتی تنظیم و
+              راه‌اندازی می‌شود.
+            </p>
+            <button
+              className="text-[18px] font-medium mt-8
+        inner-orange-shadow    px-6 py-2 rounded-lg hover:bg-[var(--dark-light-primary)] hover:text-white transition"
+            >
+              اطلاعات بیشتر
+            </button>
+          </div>
+
+          <div className="  flex flex-row-reverse  items-center justify-start h-[350px]">
+             <div className="w-[25px] h-full  bg-orange-500 " />
+            <div className="h-full flex justify-center items-center bg-[#030F3F]">
+              <p
+                className=" text-center text-[26px] px-2 font-medium text-orange-400 tracking-wider"
+                style={{ writingMode: "vertical-rl", textOrientation: "mixed" }}
+              >
+                smart vertical <br/> take_off drone
+              </p>
+            </div>
+            <div className="bg-[#020A2A] px-6 h-full w-full grid place-content-center gap-6  max-w-[434px] text-center">
+              <img src="/img1.jpg" alt="drone" className="mx-auto h-24 " />
+              <p className="text-[20px] font-normal">پهپاد عمود پرواز هوشمند</p>
+              <p className="text-[24px] font-medium text-green-600">ورژن ۱.۲</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid  md:grid-cols-2 items-center gap-8   mx-auto mb-20">
+
+          <div className="  flex  items-center justify-start h-[350px]">
+             <div className="w-[25px] h-full  bg-orange-500 " />
+            <div className="h-full flex justify-center items-center bg-[#030F3F]">
+              <p
+                className=" text-center text-[26px] px-2 font-medium text-orange-400 tracking-wider"
+                style={{ writingMode: "vertical-rl", textOrientation: "mixed" }}
+              >
+                smart vertical <br/> take_off drone
+              </p>
+            </div>
+            <div className="bg-[#020A2A] px-6 h-full w-full grid place-content-center gap-6  max-w-[434px] text-center">
+              <img src="/img1.jpg" alt="drone" className="mx-auto h-24 " />
+              <p className="text-[20px] font-normal">پهپاد عمود پرواز هوشمند</p>
+              <p className="text-[24px] font-medium text-green-600">ورژن ۱.۲</p>
+            </div>
+          </div>
+
+          <div className="w-[652px] h-[303px] px-12 py-6">
+            <h2 className="text-[32px] font-semibold mb-4">
+              پهپاد عمود پرواز هوشمند{" "}
+              <span className="text-[32px] font-semibold text-gray-400">
+                ورژن ۱.۲
+              </span>
+            </h2>
+            <p className="text-[24px] font-medium text-gray-300 leading-relaxed mb-6 mt-3 pr-6">
+              این محصول اولین نسل پهپادهای هوشمند عمودپرواز تولید شده در ایران
+              است. دارای بخش کنترل پرواز پیشرفته‌ای است که آن را برای کاربردهای
+              بسیاری از جمله امداد و نجات، مدیریت بحران، بازرسی و امنیت و
+              سامانه‌های مدار بسته و... مناسب می‌سازد. به راحتی تنظیم و
+              راه‌اندازی می‌شود.
+            </p>
+            <button
+              className="text-[18px] font-medium mt-8
+        inner-orange-shadow    px-6 py-2 rounded-lg hover:bg-[var(--dark-light-primary)] hover:text-white transition"
+            >
+              اطلاعات بیشتر
+            </button>
+          </div>
+
+
         </div>
       </section>
     </div>
