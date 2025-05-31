ea986b905522c53bfb2011d3878f172aa5ad8ac7
Ali-Asadian-456 <142652407+Ali-Asadian-456@users.noreply.github.com>
2025-05-31 12:36:16 +0330

    .

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ import { ThemeProvider } from 'next-themes'
 
 export default function App({ Component, pageProps }) {
     return (
-      <ThemeProvider attribute={"class"} defaultTheme="dark">
+      <ThemeProvider defaultTheme="dark">
       <Component {...pageProps} />
     </ThemeProvider>
   )
